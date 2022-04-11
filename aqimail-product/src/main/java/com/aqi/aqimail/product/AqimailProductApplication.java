397c7a158f934a93ff51fdbee4c537ccc6353fb7
aqi <1604062207@qq.com>
2022-04-11 21:54:33 +0800

    功能完善

--- aqimail-product/src/main/java/com/aqi/aqimail/product/AqimailProductApplication.java
+++ aqimail-product/src/main/java/com/aqi/aqimail/product/AqimailProductApplication.java
@@ -1,13 +1,29 @@
 package com.aqi.aqimail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author koi
+ * @Date 2022/4/11 20:49
+ * @Version 1.0
+ */
+/**
+ * 整合mybatis-plus
+ * 1-导入依赖
+ * 2-配置
+ *  --配置数据源
+ *  1、导入数据库驱动
+ *  2、在application.yaml配置数据源相关信息
+ *  --配置mybatis-plus
+ *  1、使用MapperScan
+ *  2、告诉mybatis-plus，sql映射文件位置
+ * */
+@MapperScan("com.aqi.aqimail.product.dao")
 @SpringBootApplication
 public class AqimailProductApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AqimailProductApplication.class, args);
     }
-
 }
--- aqimail-product/src/test/java/com/aqi/aqimail/product/AqimailProductApplicationTests.java
+++ aqimail-product/src/test/java/com/aqi/aqimail/product/AqimailProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.aqi.aqimail.product;
 
+import com.aqi.aqimail.product.entity.BrandEntity;
+import com.aqi.aqimail.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class AqimailProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("huawei");
+        brandService.save(brandEntity);
+        System.out.println("成功");
+        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        brand_id.forEach(System.out::println);
     }
 
 }
