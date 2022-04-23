7211cf48aa55dd75411e7c41a9f6be08ec2d3fda
aqi <1604062207@qq.com>
2022-04-23 21:53:11 +0800

    功能完善

--- aqimail-product/src/test/java/com/aqi/aqimail/product/AqimailProductApplicationTests.java
+++ aqimail-product/src/test/java/com/aqi/aqimail/product/AqimailProductApplicationTests.java
@@ -6,66 +6,14 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.List;
 
 @SpringBootTest
 class AqimailProductApplicationTests {
     @Autowired
     BrandService brandService;
-//    @Test
-//    public void testUpLoad() throws FileNotFoundException {
-//        String bucketName = "aqimall";
-//        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-//        String objectName = "meizu1.jpg";
-//        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-//        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-//        String filePath= "C:\\Users\\koi\\Desktop\\63d0f703918fa0ec6174ff90cf2176e43c6ddb39.jpg";
-//
-//        InputStream inputStream = new FileInputStream(filePath);
-//            // 创建PutObject请求。
-//        ossClient.putObject(bucketName, objectName, inputStream);
-//    }
-//    @Test
-//    public void testUp() throws Exception {
-//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-//        String endpoint = "oss-cn-guangzhou.aliyuncs.com";
-//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5tChWd4jahxzk5xiiyyJ";
-//        String accessKeySecret = "aWyGHrWL8ttCkkbptSOl0XI3JpexEj";
-//        // 填写Bucket名称，例如examplebucket。
-//        String bucketName = "aqimall";
-//        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-//        String objectName = "meizu.jpg";
-//        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-//        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-//        String filePath= "C:\\Users\\koi\\Desktop\\63d0f703918fa0ec6174ff90cf2176e43c6ddb39.jpg";
-//
-//        // 创建OSSClient实例。
-//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-//
-//        try {
-//            InputStream inputStream = new FileInputStream(filePath);
-//            // 创建PutObject请求。
-//            ossClient.putObject(bucketName, objectName, inputStream);
-//        } catch (OSSException oe) {
-//            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-//                    + "but was rejected with an error response for some reason.");
-//            System.out.println("Error Message:" + oe.getErrorMessage());
-//            System.out.println("Error Code:" + oe.getErrorCode());
-//            System.out.println("Request ID:" + oe.getRequestId());
-//            System.out.println("Host ID:" + oe.getHostId());
-//        } catch (ClientException ce) {
-//            System.out.println("Caught an ClientException, which means the client encountered "
-//                    + "a serious internal problem while trying to communicate with OSS, "
-//                    + "such as not being able to access the network.");
-//            System.out.println("Error Message:" + ce.getMessage());
-//        } finally {
-//            if (ossClient != null) {
-//                ossClient.shutdown();
-//            }
-//        }
-//        System.out.println("done");
-//    }
+
     @Test
     void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
